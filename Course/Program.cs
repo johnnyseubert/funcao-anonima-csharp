90d8e56bfa101542325c55a69a0aacf75b93646a
Jbseubert blasius <jbseubert@gmail.com>
2022-03-01 16:19:56 -0300

    Adicionando arquivos

---
+++ Course/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Product> lista = new List<Product>();
+            
+            lista.Add(new Product("Tv", 900.00));
+            lista.Add(new Product("Notebook", 1200.00));
+            lista.Add(new Product("Tablet", 450.00));
+            
+            lista.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach (Product item in lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
